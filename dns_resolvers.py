f70dfd0be97261ab3778d5468886fde81c032bba
Haroon Awan <mrharoonawan@gmail.com>
2023-10-29 03:25:04 +0500

    Create dns_resolvers.py

---
+++ dns_resolvers.py
@@ -0,0 +1,24 @@
+# Implement attack checks for DNS hijacking, spoofing, cache poisoning, resolver attacks
+def check_dns_anomalies(resolvers, domain_to_monitor):
+    anomalies = {"DNS Hijacking": [], "DNS Spoofing": [], "DNS Cache Poisoning": [], "DNS Resolver Spoofing": [], "DNS Resolver Cache Poisoning": [], "DNS Sequence Number Guessing": []}
+
+    for resolver in resolvers:
+        if is_dns_hijacking(resolver, domain_to_monitor, resolver.responses):
+            anomalies["DNS Hijacking"].append(resolver.ip)
+
+        if is_dns_spoofing(resolver, domain_to_monitor, resolver.responses):
+            anomalies["DNS Spoofing"].append(resolver.ip)
+
+        if is_dns_cache_poisoning(resolver, domain_to_monitor, resolver.responses):
+            anomalies["DNS Cache Poisoning"].append(resolver.ip)
+
+        if is_dns_resolver_spoofing(resolver, domain_to_monitor, resolver.responses):
+            anomalies["DNS Resolver Spoofing"].append(resolver.ip)
+
+        if is_dns_resolver_cache_poisoning(resolver, domain_to_monitor, resolver.responses):
+            anomalies["DNS Resolver Cache Poisoning"].append(resolver.ip)
+
+        if is_dns_sequence_number_guessing(resolver, domain_to_monitor, resolver.responses):
+            anomalies["DNS Sequence Number Guessing"].append(resolver.ip)
+
+    return anomalies
